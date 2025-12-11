910e74b2a47e23dd3cf75f52c7d85e56cf70ea9c
Lynx 琅邪 <lynxlangya@gmail.com>
2025-12-11 17:01:39 +0800

    Initial Commit

---
+++ Lexi/LexiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiApp.swift
+//  Lexi
+//
+//  Created by 琅邪 on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LexiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
